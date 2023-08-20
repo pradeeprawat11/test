57a87f9b7c092813506a1392ffc30fe2a9f0fca8
Your Name <pradeeprawat1717@gmail.com>
2023-08-20 09:55:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return <h1>Class Component</h1>
+  }
+}
+
+export default App;
